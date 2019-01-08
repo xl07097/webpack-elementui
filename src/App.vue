7bcfefc41bdd80c19cd17f0f744d8f3017a1e35f
xueliang <1058392650@qq.com>
2019-01-08 10:35:40 +0800

    format

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
-    <div id="app">
-        <router-link to="index">首页</router-link>
-        <router-link to="/">登录</router-link>
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-link to="index">首页</router-link>
+    <router-link to="/">登录</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: "App",
-    provide(){
-        return {
-            msg: '测试依赖与注入'
-        }
-    },
-    created(){
-
-    }
-}
+  name: "App",
+  provide() {
+    return {
+      msg: "测试依赖与注入"
+    };
+  },
+  created() {}
+};
 </script>
 
 
 <style  lang="less">
-    html,body,#app{
-        margin: 0;
-        width: 100%;
-        height: 100%;
-    }
-
+html,
+body,
+#app {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
 
