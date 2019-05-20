2b4702927418332e882c72bf1908f5ae8971c9c5
xueliang <xueliang@szusoft.com>
2019-05-20 09:49:23 +0800

    chart

--- src/components/ChartVue.vue
+++ src/components/ChartVue.vue
@@ -1,61 +1,78 @@
 <template>
-    <div :id="id" :style="{'width':width+'px','height':height+'px'}"></div>
+	<div :id="id" :style="{'width':width+'px','height':height+'px'}"></div>
 </template>
 <script>
-import * as echarts from 'echarts'
+import * as echarts from "echarts";
 export default {
-  name: 'ChartVue',
-  props: {
-    option: {
-      type: Object,
-      default: function () {
-        return {
-          title: {
-            text: 'ECharts 入门示例'
-          },
-          tooltip: {},
-          legend: {
-            data: ['销量']
-          },
-          xAxis: {
-            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-          },
-          yAxis: {},
-          series: [
-            {
-              name: '销量',
-              type: 'bar',
-              data: [5, 20, 36, 10, 10, 20]
-            }
-          ]
-        }
-      }
-    },
-    width: {
-      type: Number,
-      default: 200
-    },
-    height: {
-      type: Number,
-      default: 200
-    }
-  },
-  data: function () {
-    return {
-      id: 'echart' + ~~(Math.random() * 1000000),
-      chart: null
-    }
-  },
-  methods: {
-    init () {
-      if (!this.chart) {
-        this.chart = echarts.init(document.getElementById(this.id))
-      }
-      this.chart.setOption(this.option)
-    }
-  },
-  mounted () {
-    this.init()
-  }
-}
+  	name: "ChartVue",
+  	props: {
+    	option: {
+      		type: Object,
+      		default: function() {
+        		return {
+          			title: {
+            			text: "ECharts 入门示例"
+          			},
+          			tooltip: {},
+          			legend: {
+            			data: ["销量"]
+          			},
+          			xAxis: {
+            			data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          			},
+          			yAxis: {},
+          			series: [
+            			{
+              				name: "销量",
+              				type: "bar",
+              				data: [5, 20, 36, 10, 10, 20]
+            			}
+          			]
+        		};
+      		}
+    	},
+    	width: {
+      		type: Number,
+      		default: 200
+    	},
+    	height: {
+      		type: Number,
+      		default: 200
+    	}
+  	},
+	data() {
+		return {
+      		id: "echart" + ~~(Math.random() * 1000000),
+      		chart: null
+    	};
+  	},
+  	methods: {
+    	init() {
+      		if (!this.chart) {
+        		this.chart = echarts.init(document.getElementById(this.id));
+      		}
+      		this.drawChart();
+		},
+		drawChart(){
+			this.chart.setOption(this.option);
+		}
+  	},
+  	mounted() {
+    	this.init();
+	},
+	beforeDestroy(){
+		if(this.chart){
+			this.chart.distroy();
+			this.chart = null;
+		}
+	},
+	watch:{
+		option:{
+			handler(){
+				this.drawChart()
+			},
+			deep: true
+		}
+	}
+};
 </script>
