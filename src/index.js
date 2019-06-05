d03640d647788e76f4d2c54309a44c80437c4674
xueliang <xueliang@szusoft.com>
2019-06-05 16:06:46 +0800

    router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import Vue from 'vue';
 import router from './router/index';
 import iView from 'iview';
@@ -5,6 +6,7 @@ import iView from 'iview';
 import 'iview/dist/styles/iview.css'
 
 import App from './App';
+
 
 import 'babel-polyfill';
 // import './router/permission'
--- src/views/index.vue
+++ src/views/index.vue
@@ -11,6 +11,7 @@ export default {
             a:1,
             b:2
         }
+        new Set([1,2,3,4,5,6,7,8,4,5]);
         console.log({...obj})
     }
 }
