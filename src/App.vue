6e2344a914874051978d8ddc88baf42f64d6adf9
xueliang <xueliang@szusoft.com>
2019-07-23 09:18:25 +0800

    img

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 </template>
 <script>
     export default {
-        name: "app"
+        name: 'App'
     };
 </script>
 <style lang="scss">
-
+    @import "common.scss";
 </style>
---
+++ src/components/MessageBox/AlertModal.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="show" id="alertContent" class="modal" v-show="show">
+        <div class="alert-modal">
+            <div class="modal-header">
+                <img src="../../assets/alertModal/info.png" alt="info">
+                <span>{{title}}</span>
+            </div>
+            <div class="modal-body">
+                {{text}}
+            </div>
+            <div class="modal-footer">
+                <div class="confirm" style="margin-left: 46px;" @click="close">确定</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Alert',
+        data() {
+            return {
+                show: true
+            };
+        },
+        methods: {
+            close() {
+                this.show = false;
+                this.onOk && this.onOk();
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import "msgBoxCommon";
+</style>
