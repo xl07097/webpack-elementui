30af38867758932933275b697d2502fdfa268ee8
xueliang <xueliang@szusoft.com>
2019-09-18 11:46:47 +0800

    Update index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,7 @@
     <el-button type="primary" @click="add">count</el-button>
     <!-- <img src="/static/image/163-1.png" alt="163" width="100" /> -->
     <!-- <span style="color:red;font-size:20px;">{{"★★★★★☆☆☆☆☆".slice(5 - 2, 10 - 2)}}</span> -->
-
+    <span class="add" v-show="show">哈哈哈</span>
   </div>
 </template>
 <script>
@@ -12,6 +12,11 @@ import { mapState, mapMutations } from "vuex";
 
 export default {
   name: "Index",
+  data(){
+      return {
+          show: true
+      }
+  },
   computed: {
     ...mapState(["count"]),
     ...mapState({
@@ -19,7 +24,10 @@ export default {
     })
   },
   methods: {
-    ...mapMutations(["add"]),
+    // ...mapMutations(["add"]),
+    add(){
+        this.show = !this.show;
+    },
     userList(){
         fetch("http://localhost:9087/note/user/getById?id=1")
         .then(data => data.json())
@@ -38,6 +46,9 @@ export default {
 <style lang="less">
 .dd {
   position: absolute;
+}
+.add{
+    display:none;
 }
 </style>
 
