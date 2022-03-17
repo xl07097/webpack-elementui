c4552f2dd77679897b9b7bbd4a1456dbac341852
xueliang <1058392650@qq.com>
2022-03-17 10:00:40 +0800

    Create storage.js

---
+++ src/utils/storage.js
@@ -0,0 +1,40 @@
+class MyStorage{
+
+  storage;
+
+  prefix;
+
+  constructor(storage=window.sessionStorage, prefix='erp_') {
+    this.storage = storage
+    this.prefix = prefix
+  }
+
+  clear() {
+    this.storage.clear()
+  }
+  
+  getItem(key) {
+    const value = this.storage.getItem(`${this.prefix}${key}`)
+    try {
+      return JSON.parse(value).data
+    } catch (e) {
+      return null
+    }
+  }
+
+  removeItem(key) {
+    this.storage.removeItem(`${this.prefix}${key}`)
+  }
+
+  setItem(key, value) {
+    this.storage.setItem(`${this.prefix}${key}`, JSON.stringify({
+      data: value,
+      time: Date.now()
+    }))
+  }
+
+}
+
+export default MyStorage
+
+export const storage = new MyStorage()
