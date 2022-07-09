16ea2c2342e2d7448659a445de8bb1e15c055985
xueliang <1058392650@qq.com>
2022-07-09 19:16:58 +0800

    Update saveFile.js

--- src/http/saveFile.js
+++ src/http/saveFile.js
@@ -3,14 +3,13 @@
  * @returns
  */
 export const getExportName = (res) => {
-  const { headers } = res;
-  return decodeURIComponent(headers['content-disposition'].replace(/\w+;filename=(.*)/, '$1'));
-};
+  const { headers } = res
+  return decodeURIComponent(headers['content-disposition'].replace(/\w+;filename=(.*)/, '$1'))
+}
 
-
-  // let blob = new Blob([data], {
-  //     type: type                 // 'application/vnd.ms-excel'
-  // })
+// let blob = new Blob([data], {
+//     type: type                 // 'application/vnd.ms-excel'
+// })
 
 /**
  * @param {*} blob blob数据
@@ -19,26 +18,25 @@ export const getExportName = (res) => {
 export const saveFile = (blob, filename) => {
   if ('msSaveOrOpenBlob' in navigator) {
     // 兼容edge
-    window.navigator.msSaveOrOpenBlob(blob, filename);
-  }
-  if (window.navigator && window.navigator.msSaveBlob) {
-    window.navigator.msSaveBlob(blob, filename);
+    window.navigator.msSaveOrOpenBlob(blob, filename)
+  } else if (window.navigator && window.navigator.msSaveBlob) {
+    window.navigator.msSaveBlob(blob, filename)
   } else {
     /* 火狐谷歌的文件下载方式 */
-    let a = document.createElement('a');
-    let href = window.URL.createObjectURL(blob);
-    a.href = href;
-    a.download = filename;
-    document.body.appendChild(a);
+    let a = document.createElement('a')
+    let href = window.URL.createObjectURL(blob)
+    a.href = href
+    a.download = filename
+    document.body.appendChild(a)
     let evt = new MouseEvent('click', {
       bubbles: false,
       cancelable: true,
       view: window,
-    });
-    a.dispatchEvent(evt);
-    window.URL.revokeObjectURL(href);
+    })
+    a.dispatchEvent(evt)
+    window.URL.revokeObjectURL(href)
     setTimeout(() => {
-      document.body.removeChild(a);
-    }, 100);
+      document.body.removeChild(a)
+    }, 100)
   }
-};
+}
