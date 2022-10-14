150d731b127a597a39ae9eb387cd91be4ff0a0ed
xueliang <1058392650@qq.com>
2022-10-14 17:42:11 +0800

    依赖

--- src/views/monitorCenter/operationlog/index.vue
+++ src/views/monitorCenter/operationlog/index.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    operationlog
+    <el-upload :headers="{token}" action="/note/upload/file" :file-list="fileList" name="upfile" :on-success="onSuccess">
+      <el-button>上传</el-button>
+    </el-upload>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'OperationLog'
+  name: 'OperationLog',
+  data(){
+    return {
+      fileList: [],
+      token: ''
+    }
+  },
+  mounted(){
+    this.token = sessionStorage.getItem('token')
+  },
+  methods:{
+    onSuccess(res){
+      this.fileList.push({
+        name: 'jj',
+        url: res.data
+      })
+      
+    }
+  }
 }
 </script>
 
