9a052091d671d200a2d848e6902cfbe169859921
xueliang <1058392650@qq.com>
2022-11-08 17:24:39 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,24 @@ export default {
   components: {
     AppHeader
   },
+  mounted() {
+    window.addEventListener('error', (eventErr) => {
+      const filename = eventErr.filename || ''
+      if (/(.css|.js)$/.test(filename)) {
+        if (this.hasDialog) {
+          return
+        }
+        this.hasDialog = true
+        this.$alert('因版本更新，页面需重新载入以加载对应资源！', '提示', {
+          confirmButtonText: '确定',
+          callback: () => {
+            window.location.reload(true)
+          },
+        })
+      }
+      eventErr.preventDefault()
+    }, true)
+  },
 };
 </script>
 <style lang="scss">
--- src/importComponent.js
+++ src/importComponent.js
@@ -1,165 +1,178 @@
-import Vue from 'vue';
+import Vue from 'vue'
 import {
-  Alert,
-  Aside,
+  CollapseTransition,
+  Loading,
+  Pagination,
+  Dialog,
   Autocomplete,
-  Backtop,
-  Badge,
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
-  ButtonGroup,
-  Calendar,
-  Card,
-  Carousel,
-  CarouselItem,
-  Cascader,
-  CascaderPanel,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
   Checkbox,
   CheckboxButton,
   CheckboxGroup,
-  Col,
-  Collapse,
-  CollapseItem,
-  ColorPicker,
-  Container,
-  DatePicker,
-  Dialog,
-  Divider,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  Footer,
-  Form,
-  FormItem,
-  Header,
-  Icon,
-  Image,
-  Input,
-  InputNumber,
-  Link,
-  Loading,
-  Main,
-  Menu,
-  MenuItem,
-  MenuItemGroup,
+  Switch,
+  Select,
   Option,
   OptionGroup,
-  PageHeader,
-  Pagination,
-  Popover,
-  Progress,
-  Radio,
-  RadioButton,
-  RadioGroup,
-  Rate,
-  Row,
-  Select,
-  Slider,
-  Spinner,
-  Step,
-  Steps,
-  Submenu,
-  Switch,
+  Button,
+  ButtonGroup,
   Table,
   TableColumn,
-  TabPane,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
+  Popover,
+  Tooltip,
+  MessageBox,
+  Breadcrumb,
+  BreadcrumbItem,
+  Form,
+  FormItem,
   Tabs,
+  TabPane,
   Tag,
+  Tree,
+  Alert,
+  Notification,
+  Slider,
+  Icon,
+  Row,
+  Col,
+  Upload,
+  Progress,
+  Spinner,
+  Message,
+  Badge,
+  Card,
+  Rate,
+  Steps,
+  Step,
+  Carousel,
+  Scrollbar,
+  CarouselItem,
+  Collapse,
+  CollapseItem,
+  Cascader,
+  ColorPicker,
+  Transfer,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
   Timeline,
   TimelineItem,
-  TimePicker,
-  TimeSelect,
-  Tooltip,
-  Transfer,
-  Tree,
-  Upload,
-  MessageBox,
-  Scrollbar,
-} from 'element-ui';
+  Link,
+  Divider,
+  Image,
+  Calendar,
+  Backtop,
+  InfiniteScroll,
+  PageHeader,
+  CascaderPanel,
+  Avatar,
+  Drawer,
+  Popconfirm,
+  Skeleton,
+  SkeletonItem,
+  Empty,
+  Descriptions,
+  DescriptionsItem,
+  Result,
+} from 'element-ui'
 
-Vue.use(Pagination);
-Vue.use(Dialog);
-Vue.use(Autocomplete);
-Vue.use(Dropdown);
-Vue.use(DropdownMenu);
-Vue.use(DropdownItem);
-Vue.use(Menu);
-Vue.use(Submenu);
-Vue.use(MenuItem);
-Vue.use(MenuItemGroup);
-Vue.use(Input);
-Vue.use(InputNumber);
-Vue.use(Radio);
-Vue.use(RadioGroup);
-Vue.use(RadioButton);
-Vue.use(Checkbox);
-Vue.use(CheckboxButton);
-Vue.use(CheckboxGroup);
-Vue.use(Switch);
-Vue.use(Select);
-Vue.use(Option);
-Vue.use(OptionGroup);
-Vue.use(Button);
-Vue.use(ButtonGroup);
-Vue.use(Table);
-Vue.use(TableColumn);
-Vue.use(DatePicker);
-Vue.use(TimeSelect);
-Vue.use(TimePicker);
-Vue.use(Popover);
-Vue.use(Tooltip);
-Vue.use(Breadcrumb);
-Vue.use(BreadcrumbItem);
-Vue.use(Form);
-Vue.use(FormItem);
-Vue.use(Tabs);
-Vue.use(TabPane);
-Vue.use(Tag);
-Vue.use(Tree);
-Vue.use(Alert);
-Vue.use(Slider);
-Vue.use(Icon);
-Vue.use(Row);
-Vue.use(Col);
-Vue.use(Upload);
-Vue.use(Progress);
-Vue.use(Spinner);
-Vue.use(Badge);
-Vue.use(Card);
-Vue.use(Rate);
-Vue.use(Steps);
-Vue.use(Step);
-Vue.use(Carousel);
-Vue.use(CarouselItem);
-Vue.use(Collapse);
-Vue.use(CollapseItem);
-Vue.use(Cascader);
-Vue.use(ColorPicker);
-Vue.use(Transfer);
-Vue.use(Container);
-Vue.use(Header);
-Vue.use(Aside);
-Vue.use(Main);
-Vue.use(Footer);
-Vue.use(Timeline);
-Vue.use(TimelineItem);
-Vue.use(Link);
-Vue.use(Divider);
-Vue.use(Image);
-Vue.use(Calendar);
-Vue.use(Backtop);
-Vue.use(PageHeader);
-Vue.use(CascaderPanel);
-Vue.use(Scrollbar);
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Autocomplete)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(Checkbox)
+Vue.use(CheckboxButton)
+Vue.use(CheckboxGroup)
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(OptionGroup)
+Vue.use(Button)
+Vue.use(ButtonGroup)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(DatePicker)
+Vue.use(TimeSelect)
+Vue.use(TimePicker)
+Vue.use(Popover)
+Vue.use(Tooltip)
+// Vue.use(Breadcrumb)
+// Vue.use(BreadcrumbItem)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Alert)
+// Vue.use(Slider)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Upload)
+Vue.use(Progress)
+Vue.use(Spinner)
+// Vue.use(Badge)
+Vue.use(Card)
+// Vue.use(Rate)
+// Vue.use(Steps)
+// Vue.use(Step)
+// Vue.use(Carousel)
+// Vue.use(CarouselItem)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+// Vue.use(Cascader)
+// Vue.use(ColorPicker)
+// Vue.use(Transfer)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+// Vue.use(Footer)
+// Vue.use(Timeline)
+// Vue.use(TimelineItem)
+// Vue.use(Link)
+// Vue.use(Divider)
+// Vue.use(Image)
+// Vue.use(Calendar)
+// Vue.use(Backtop)
+// Vue.use(PageHeader)
+// Vue.use(CascaderPanel)
+Vue.use(Scrollbar)
 
-Vue.use(Loading.directive);
+Vue.use(Loading.directive)
 
-Vue.prototype.$ELEMENT = { size: 'small', zIndex: 1000 };
-Vue.prototype.$loading = Loading.service;
-// Vue.prototype.$msgbox = MessageBox;
-Vue.prototype.$alert = MessageBox.alert;
-Vue.prototype.$confirm = MessageBox.confirm;
-// Vue.prototype.$prompt = MessageBox.prompt;
-// Vue.prototype.$notify = Notification;
-// Vue.prototype.$message = Message;
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 1000 }
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$notify = Notification
+Vue.prototype.$message = Message
