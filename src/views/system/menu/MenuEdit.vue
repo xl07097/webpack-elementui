d3b59b432d4f370d26cfacebe5cdf8bc27654580
xueliang <1058392650@qq.com>
2023-04-04 20:35:10 +0800

    Update MenuEdit.vue

--- src/views/system/menu/MenuEdit.vue
+++ src/views/system/menu/MenuEdit.vue
@@ -6,29 +6,36 @@
     @open="open"
     @close="close"
   >
-    <el-form
-      ref="form"
-      :model="form"
-      :rules="rules"
-      label-width="120px"
-    >
+    <el-form ref="form" :model="form" :rules="rules" label-width="110px">
       <el-form-item label="名称" prop="title">
         <el-input v-model="form.title" />
       </el-form-item>
       <el-form-item label="类别" prop="nodeType">
-        <el-radio-group v-model="form.nodeType" :disabled="operType==='edit'" size="small">
+        <el-radio-group
+          v-model="form.nodeType"
+          :disabled="operType === 'edit'"
+          size="small"
+        >
           <el-radio-button :label="1">目录</el-radio-button>
           <el-radio-button :label="2">菜单</el-radio-button>
           <el-radio-button :label="3">按钮</el-radio-button>
         </el-radio-group>
       </el-form-item>
-      <el-form-item v-if="form.nodeType!==3" label="路径" prop="path">
+      <el-form-item v-if="form.nodeType !== 3" label="路径" prop="path">
         <el-input v-model="form.path" />
       </el-form-item>
-      <el-form-item v-if="form.nodeType!==3" label="文件路径" prop="component">
+      <el-form-item
+        v-if="form.nodeType !== 3"
+        label="文件路径"
+        prop="component"
+      >
         <el-input v-model="form.component" />
       </el-form-item>
-      <el-form-item v-if="form.nodeType===3" label="按钮标志" prop="functionName">
+      <el-form-item
+        v-if="form.nodeType === 3"
+        label="按钮标志"
+        prop="functionName"
+      >
         <el-input v-model="form.functionName" />
       </el-form-item>
       <el-form-item label="排序" prop="sortNum">
@@ -50,7 +57,9 @@
 
     <div class="demo-drawer__footer">
       <el-button @click="close">取 消</el-button>
-      <el-button type="primary" :loading="loading" @click="onSubmit">{{ loading ? '提交中 ...' : '确 定' }}</el-button>
+      <el-button type="primary" :loading="loading" @click="onSubmit">
+        {{ loading ? '提交中 ...' : '确 定' }}
+      </el-button>
     </div>
   </el-drawer>
 </template>
@@ -58,51 +67,51 @@
 <script>
 import { post } from '@/http/request'
 export default {
-  props:{
-    detail:{
+  props: {
+    detail: {
       type: Object,
-      default(){
+      default() {
         return {}
-      }
+      },
     },
-    operType:{
+    operType: {
       type: String,
-      validator(value){
+      validator(value) {
         return ['add', 'edit', 'see'].includes(value)
       },
-      default: 'add'
+      default: 'add',
     },
-    visible:{
+    visible: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
-  data(){
+  data() {
     return {
-      form:{},
+      form: {},
       rules: Object.freeze({
-        title: {required: true, message: '必填', trigger: 'blur'},
-        nodeType: {required: true, message: '必选', trigger: 'change'},
-        path: {required: true, message: '必填', trigger: 'blur'},
-        functionName: {required: true, message: '必填', trigger: 'blur'},
+        title: { required: true, message: '必填', trigger: 'blur' },
+        nodeType: { required: true, message: '必选', trigger: 'change' },
+        path: { required: true, message: '必填', trigger: 'blur' },
+        functionName: { required: true, message: '必填', trigger: 'blur' },
       }),
       drawer: false,
-      loading: false
+      loading: false,
     }
   },
-  methods:{
-    open(){
+  methods: {
+    open() {
       this.form = Object.assign({}, this.detail)
     },
-    onSubmit(){
-      this.$refs.form.validate(isValid => {
+    onSubmit() {
+      this.$refs.form.validate((isValid) => {
         if (!isValid) return
         this.loading = true
         let url = '/sysMenu/update'
-        if(this.operType === 'add'){
+        if (this.operType === 'add') {
           url = '/sysMenu/add'
         }
-        post(url, this.form).then(res => {
+        post(url, this.form).then((res) => {
           this.loading = false
           if (res.code === 200) {
             this.$message.success('操作成功')
@@ -112,24 +121,24 @@ export default {
         })
       })
     },
-    close(){
+    close() {
       this.$emit('update:visible', false)
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style lang="scss">
-.el-drawer__body:has(>.demo-drawer__footer){
+.el-drawer__body:has(> .demo-drawer__footer) {
   margin-bottom: 60px;
   overflow-y: auto;
 }
-.demo-drawer__footer{
-    position: fixed;
-    bottom: 0;
-    /* width: 100%; */
-    text-align: right;
-    right: 0;
-    padding: 10px 20px;
+.demo-drawer__footer {
+  position: fixed;
+  bottom: 0;
+  /* width: 100%; */
+  text-align: right;
+  right: 0;
+  padding: 10px 20px;
 }
 </style>
