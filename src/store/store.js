7d26c9682b1083a8bdf54ddbef98b7dde9a684c4
xueliang <1058392650@qq.com>
2023-04-17 22:32:51 +0800

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,28 +1,29 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import permission from './modules/permission';
-import app from './modules/app';
-import tagsView from './modules/tagsView';
-import {storage} from '@/utils/storage';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { storage } from '@/utils/storage'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+const context = require.context('./modules', false, /\.js$/)
+const modules = context.keys().reduce((acc, key) => {
+  const k = key.replace(/(\.\/|\.js)/g, '')
+  acc[k] = context(key).default
+  return acc
+}, {})
+console.log(modules)
 
 export default new Vuex.Store({
   state: {
     // 状态树   辅助函数 mapState
-    loginUser: storage.getItem('loginUser') || {}
+    loginUser: storage.getItem('loginUser') || {},
   },
   getters: {},
   mutations: {
-    setLoginUser(state, payload){
+    setLoginUser(state, payload) {
       storage.setItem('loginUser', payload)
       state.loginUser = payload
-    }
+    },
   },
-  modules: {
-    // 模块化
-    permission,
-    app,
-    tagsView
-  },
-});
+  // 模块化
+  modules: modules,
+})
