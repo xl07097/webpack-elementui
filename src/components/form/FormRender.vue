4cd327f4a80b4830979a105ed8c1fa5dc3bef0d9
xueliang <1058392650@qq.com>
2023-04-18 11:07:53 +0800

    查询字段自定义配置

--- src/components/form/FormRender.vue
+++ src/components/form/FormRender.vue
@@ -7,9 +7,9 @@
     inline
   >
     <el-form-item
-      v-for="(field, index) of fields"
+      v-for="(field, index) of filnalFields"
       :key="field.prop"
-      :class="{ 'form-item-hide': showAll ? false : index > showNumber }"
+      :class="{ 'form-item-hide': showAll ? false : index > showNumber-1 }"
       :label="field.label"
       :label-width="field.labelWidth"
       :prop="field.prop"
@@ -22,33 +22,41 @@
         :width="width"
         :config="field.config"
       />
-      <span class="inner-label">{{ field.label }}</span>
+      <!-- <span class="inner-label">{{ field.label }}</span> -->
     </el-form-item>
 
     <el-form-item class="search-form-btn">
       <el-button
-        v-if="showNumber<fields.length"
-        :icon="`${showAll?'el-icon-arrow-up':'el-icon-arrow-down'}`"
+        v-if="showNumber<filnalFields.length"
         type="text"
         @click="expand"
       >
         {{ showAll ? '收起':'展开' }}
+        <i v-if="showAll" class="el-icon-arrow-up" />
+        <i v-else class="el-icon-arrow-down" />
       </el-button>
-      <el-button type="primary" icon="el-icon-search" @click="search">
+      <el-button type="primary" @click="search">
         查询
       </el-button>
-      <el-button icon="el-icon-refresh-right" @click="reset">
+      <el-button @click="reset">
         重置
       </el-button>
+      <SearchSetting
+        :columns="fields"
+        :active-name="activeName"
+        @confirm="confirm"
+      />
+      <!-- <el-button type="text" @click="configuration">自定义配置</el-button> -->
     </el-form-item>
   </el-form>
 </template>
 <script>
 import ItemRender from './ItemRender'
+import SearchSetting from './SearchSetting.vue'
 
 export default {
   name: 'FormRender',
-  components: { ItemRender },
+  components: { ItemRender, SearchSetting },
   props: {
     value: {
       type: Object,
@@ -73,16 +81,20 @@ export default {
     widthConfig:{
       type: Object,
       default(){
-        return {
-
-        }
+        return {}
       }
+    },
+    activeName:{
+      type: String,
+      default: '',
     }
   },
   data() {
     return {
       showNumber: 10,
       showAll: false,
+      filnalFields: [],
+      refreshFlag: false
     }
   },
   computed: {
@@ -96,11 +108,13 @@ export default {
     },
   },
   mounted() {
-    const searchWidth = document.querySelector('.search-form').getBoundingClientRect().width
-    this.handleWidth(searchWidth)
+    // this.initWidth()
     this.init()
   },
   methods: {
+    getWidth(){
+      return document.querySelector('.search-form').getBoundingClientRect().width
+    },
     init() {
       let timer = null
       const observe = new ResizeObserver((entries) => {
@@ -117,10 +131,15 @@ export default {
         observe.disconnect()
       })
     },
+    initWidth(){
+      const searchWidth = this.getWidth()
+      this.handleWidth(searchWidth)
+    },
     handleWidth(searchWidth){
-      if (this.showAll) {
+      if (this.showAll && !this.refreshFlag) {
         return
       }
+      this.refreshFlag = false
       // const widthMap = Object.assign({
       //   input: 200,
       //   select: 200,
@@ -130,16 +149,16 @@ export default {
       //   week: 200,
       //   daterange: 200,
       // }, this.widthConfig) 
-
-      const fields = this.fields
-      let totalWidth = 230
+      const fields = this.filnalFields
+      // 初始按钮区域宽度
+      let totalWidth = 260
       for (let index = 0; index < fields.length; index++) {
         const item = fields[index]
         const labelWidth = item.labelWidth || this.labelWidth
         const width = item.width || this.width // widthMap[item.type]
         const margin = 10
         if (totalWidth + labelWidth + width + margin > searchWidth) {
-          this.showNumber = index - 1
+          this.showNumber = index 
           return
         }
         totalWidth = totalWidth + labelWidth + width + margin
@@ -149,7 +168,7 @@ export default {
       this.showAll = !this.showAll
     },
     search(){
-      const fields = this.fields
+      const fields = this.filnalFields
       let data = {}
       const value = this.value
       fields.forEach(item => {
@@ -161,6 +180,7 @@ export default {
           data[item.prop] = value[item.prop]
         }
       })
+      console.log(data)
       this.$emit('search', data)
     },
     reset(){
@@ -175,7 +195,17 @@ export default {
         // }
       })
       this.currentData = data
-    }
+    },
+    // configuration(){
+
+    // },
+    confirm(fields) {
+      this.filnalFields = Object.freeze(fields)
+      this.refreshFlag = true
+      this.$nextTick(() => {
+        this.initWidth()
+      })
+    },
   },
 }
 </script>
@@ -184,6 +214,7 @@ export default {
 .search-form {
   display: flex;
   flex-wrap: wrap;
+  padding: 10px 0 6px;
 
   .el-form-item {
     margin-bottom: 10px;
---
+++ src/components/form/SearchSetting.vue
@@ -0,0 +1,244 @@
+<template>
+  <el-popover
+    placement="bottom-end"
+    title=""
+    width="240"
+    trigger="click"
+    popper-class="table-column-popover"
+    content="这是一段内容"
+  >
+    <el-checkbox
+      v-model="all"
+      class="table-column-all-check"
+      label="全选"
+      @change="changeAll"
+    />
+    <el-checkbox-group
+      v-model="checkList"
+      class="table-column-filter"
+      size="mini"
+      @change="changeSingle"
+    >
+      <draggable
+        v-model="columnsList"
+        scroll
+        handle=".dargBtn"
+        filter=".undraggable"
+      >
+        <transition-group>
+          <div
+            v-for="item of columnsList"
+            :key="item.prop"
+            style="
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+            "
+          >
+            <el-checkbox :label="item.prop">
+              {{ item.label }}
+            </el-checkbox>
+            <span
+              class="el-icon-sort dargBtn"
+              style="font-size: 20px; cursor: move"
+            />
+          </div>
+        </transition-group>
+      </draggable>
+    </el-checkbox-group>
+    <div class="table-column-popover-footer">
+      <el-button type="text" @click="reset">还原</el-button>
+      <el-button type="text" @click="confirm(true)">确定</el-button>
+    </div>
+    <el-button slot="reference" type="text">自定义配置</el-button>
+  </el-popover>
+</template>
+
+<script>
+import { baseStorage } from '@/utils/storage'
+import draggable from 'vuedraggable'
+import { deepClone } from '@/utils/commons'
+export default {
+  components: { draggable },
+  props: {
+    columns: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+    activeName:{
+      type: String,
+      default: '',
+    }
+  },
+  data() {
+    return {
+      visible: false,
+      checkList: [],
+      all: true,
+      columnsList: [],
+      prefix: 'search_',
+    }
+  },
+  computed: {
+    key() {
+      return this.$route.path.split('/').at(-1)
+    },
+  },
+  watch:{
+    columns(){
+      this.init()
+      this.confirm()
+    }
+  },
+  created() {
+    this.init()
+    this.confirm()
+  },
+  methods: {
+    init(){
+      const storageColumns = baseStorage.getItem(`${this.prefix}${this.key}${this.activeName}`) || []
+      const columns = this.columns
+
+      if (storageColumns.length === 0) {
+        this.columnsList = deepClone(columns)
+        this.checkList = columns.map((item) => item.prop)
+      } else {
+        const originFields = columns.map((item) => item.prop)
+        columns.forEach((item, index) => {
+        // debugger
+          const findIndex = storageColumns.findIndex(
+            (sf) => sf.prop === item.prop
+          )
+          if (findIndex === -1) {
+            if (index === 0) {
+              storageColumns.unshift({
+                ...item,
+                select: true,
+              })
+            } else {
+              const preItem = columns[index - 1]
+              const findPreIndex = storageColumns.findIndex(
+                (sf) => sf.prop === preItem.prop
+              )
+              storageColumns.splice(findPreIndex + 1, 0, {
+                ...item,
+                select: true,
+              })
+            }
+          }
+        })
+
+        const filterColumns = storageColumns.filter((item) => {
+          return originFields.includes(item.prop)
+        })
+        this.columnsList = filterColumns.map((item) => {
+          return columns.find((cln) => cln.prop === item.prop)
+        })
+        this.checkList = filterColumns
+          .filter((item) => item.select)
+          .map((item) => item.prop)
+        this.all = this.checkList.length === filterColumns.length
+      }
+    },
+    changeAll(flag) {
+      if (flag) {
+        this.checkList = this.columnsList.map((item) => item.prop)
+      } else {
+        this.checkList = []
+      }
+    },
+    changeSingle(rows) {
+      this.all = rows.length === this.columnsList.length
+    },
+    reset() {},
+    confirm(flag) {
+      this.$nextTick(() => {
+        document.body.click()
+      })
+      const checkList = this.checkList
+      this.columnsList.forEach((item) => {
+        item.select = checkList.includes(item.prop)
+      })
+      const columns = this.columnsList.filter((item) =>
+        checkList.includes(item.prop)
+      )
+      let stage = this.columnsList.map(item => {
+        return {
+          'label': item.label,
+          'prop': item.prop,
+          'select': item.select,
+        }
+      })
+      if(flag == true){
+        baseStorage.setItem(`${this.prefix}${this.key}${this.activeName}`, stage)
+        this.$emit('confirm', columns)
+
+      }else {
+        if(stage.length > 0){
+          baseStorage.setItem(`${this.prefix}${this.key}${this.activeName}`, stage)
+          this.$emit('confirm', columns)
+        }
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.table-column-popover {
+  padding: 0;
+  .table-column-all-check {
+    display: flex;
+    align-items: center;
+    height: 44px;
+    padding: 0 1em;
+    margin-bottom: 4px;
+    border-bottom: 1px solid #dadce0;
+  }
+  .table-column-filter {
+    display: flex;
+    flex-direction: column;
+    max-height: 300px;
+    overflow-y: auto;
+    .el-checkbox {
+      flex: 1;
+      margin: 0;
+      display: flex;
+      align-items: center;
+      height: 34px;
+      padding: 0 1em;
+    }
+    .el-checkbox:hover {
+      background: #f5f7fa;
+      cursor: pointer;
+    }
+    .el-checkbox:active {
+      cursor: pointer;
+    }
+    .dargBtn {
+      width: 40px;
+      align-self: stretch;
+      vertical-align: middle;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .dargBtn:hover {
+      background: #f5f7fa;
+    }
+  }
+  .table-column-popover-footer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 4px;
+    border-top: 1px solid #dadce0;
+    .el-button {
+      padding: 10px 16px;
+      font-size: 14px;
+      flex: 1;
+    }
+  }
+}
+</style>
