4526ba4b83292950c93d7e7d8fd69da5bbb2ec1a
xueliang <1058392650@qq.com>
2023-08-11 16:32:43 +0800

    office

--- src/views/message/signatureCheck/index.vue
+++ src/views/message/signatureCheck/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
+    <!-- <PdfOffice /> -->
     <el-input style="width:200px;">
       <template #append>
         <el-button type="primary">哈哈哈</el-button>
       </template>
     </el-input>
+    <embed
+      name="070DCB5263B13EFD6C26499FD14424FE"
+      width="100%"
+      height="100%"
+      src="https://files.zhiqiuge.com/xiangshuye/31.pdf"
+      type="application/pdf"
+      internalid="070DCB5263B13EFD6C26499FD14424FE"
+    >
   </div>
 </template>
 
@@ -14,9 +23,11 @@
 import { divide } from 'mathjs'
 import Decimal from 'decimal.js'
 
+import PdfOffice from '@/components/office/pdf/PdfOffice.vue'
 
 export default {
   name: 'SignatureCheck',
+  components:{PdfOffice},
   data(){
     return {
 
