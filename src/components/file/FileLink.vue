eae366baf4af393b9724fb23c4076c0b8c463e0f
xueliang <1058392650@qq.com>
2023-09-20 17:10:07 +0800

    file

---
+++ src/components/file/FileLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-link 
+    :href="`${OSSURIPrefix}${url}`"
+    target="_blank"
+    icon="el-icon-document"
+    :download="name"
+  >
+    {{ name }}
+  </el-link>
+</template>
+
+<script>
+// import { OSSURIPrefix } from '@/http/urls'
+export default {
+  props: {
+    url: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      OSSURIPrefix: '',
+    }
+  },
+}
+</script>
---
+++ src/components/file/FileList.vue
@@ -0,0 +1,87 @@
+<template>
+  <ul class="sh-upload-wrap">
+    <li v-for="(item, index) of list" :key="index" class="sh-upload-filelist">
+      <FileLink
+        class="sh-file-name"
+        :url="`${item.url || item.urlPath}`"
+        :name="item.name || item.fileName"
+      />
+      &emsp;&emsp;
+      <ElLink
+        v-if="operType != 'see'"
+        icon="el-icon-delete"
+        type="danger"
+        :underline="false"
+        @click="dels(index)"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import FileLink from './FileLink.vue'
+export default {
+  name: 'FileList',
+  components: { FileLink },
+  props: {
+    fileList: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+    operType: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'client', // serve 服务端删除
+    },
+  },
+  computed: {
+    list: {
+      get() {
+        return this.fileList
+      },
+      set(val) {
+        this.$emit('update:fileList', val)
+      },
+    },
+  },
+  methods: {
+    dels(index) {
+      if (this.type === 'client') {
+        const item = this.list.splice(index, 1)
+        this.$emit('trash', {
+          item,
+        })
+      } else {
+        this.$emit('trash', {
+          index,
+        })
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.sh-upload-wrap {
+  flex: 1;
+  .sh-upload-filelist {
+    display: flex;
+    align-items: center;
+    line-height: 28px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    .sh-file-name {
+      line-height: 1;
+      word-break: break-all;
+    }
+  }
+  .el-link {
+    flex: none;
+  }
+}
+</style>
