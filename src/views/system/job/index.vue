8c3fa51ddb5830312ab6f86cdc6977c18836990b
xueliang <1058392650@qq.com>
2024-03-06 20:48:36 +0800

    Create index.vue

---
+++ src/views/system/job/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>lplplp</div>
+</template>
+
+<script>
+export default {
+  name: 'SysJob',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+}
+</script>
